f49dade4918792cd474022ecfa7113aba4756da5
GrGrimales <grojasgrimales@gmail.com>
2022-01-18 12:13:08 -0500

    Se modifico ruta de img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
-import imagen from "./cryptomonedas.png";
+import imagen from "../cryptomonedas.png";
 import axios from "axios";
 
 import "./App.css";
